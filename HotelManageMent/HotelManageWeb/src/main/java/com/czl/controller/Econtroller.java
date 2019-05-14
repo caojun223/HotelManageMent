e86aa825387419e328f4282d3edb4f4191a28481
caojun223 <547623645@qq.com>
2019-05-14 20:09:53 +0800

    first

--- HotelManageMent/HotelManageWeb/src/main/java/com/czl/controller/Econtroller.java
+++ HotelManageMent/HotelManageWeb/src/main/java/com/czl/controller/Econtroller.java
@@ -2,6 +2,7 @@ package com.czl.controller;
 
 import java.util.List;
 
+import com.czl.util.LayuiUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,28 @@ import com.czl.service.Eservice;
 
 @Controller
 public class Econtroller {
-	@Autowired
+
+	@Autowired(required = false)
 	private Eservice eservice;
+
 	@RequestMapping("getAllEmp")
 	@ResponseBody
 	public List<Employee> getAllEmp(){
-		
 		return eservice.getAllEmployee();
-		
+	}
+
+	@RequestMapping("/getLayuiEmp")
+	@ResponseBody
+	public LayuiUtil<Employee> getLayuiEmp(){
+
+		LayuiUtil<Employee> util=new LayuiUtil<>();
+		util.setCode(0);
+		util.setMsg("");
+		List<Employee> allEmployee = eservice.getAllEmployee();
+		util.setCount(allEmployee.size());
+		util.setData(allEmployee);
+
+		return util;
 	}
 	
 	
