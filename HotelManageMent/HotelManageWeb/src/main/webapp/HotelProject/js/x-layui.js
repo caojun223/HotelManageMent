c1ed77bff8a1738098b1b39f5524a02ccc31b40a
caojun223 <547623645@qq.com>
2019-05-15 23:13:21 +0800

    second

--- HotelManageMent/HotelManageWeb/src/main/webapp/HotelProject/js/x-layui.js
+++ HotelManageMent/HotelManageWeb/src/main/webapp/HotelProject/js/x-layui.js
@@ -7,33 +7,33 @@
 	w		弹出层宽度（缺省调默认值）
 	h		弹出层高度（缺省调默认值）
 */
-function x_admin_show(title, url, w, h) {
-	if(title == null || title == '') {
-		title = false;
+function x_admin_show(title,url,w,h){
+	if (title == null || title == '') {
+		title=false;
 	};
-	if(url == null || url == '') {
-		url = "404.html";
+	if (url == null || url == '') {
+		url="404.html";
 	};
-	if(w == null || w == '') {
-		w = 800;
+	if (w == null || w == '') {
+		w=800;
 	};
-	if(h == null || h == '') {
-		h = ($(window).height() - 50);
+	if (h == null || h == '') {
+		h=($(window).height() - 50);
 	};
 	layer.open({
 		type: 2,
-		area: [w + 'px', h + 'px'],
+		area: [w+'px', h +'px'],
 		fix: false, //不固定
 		maxmin: true,
 		shadeClose: true,
-		shade: 0.4,
+		shade:0.4,
 		title: title,
 		content: url
 	});
 }
 
 /*关闭弹出框口*/
-function x_admin_close() {
+function x_admin_close(){
 	var index = parent.layer.getFrameIndex(window.name);
 	parent.layer.close(index);
 }
